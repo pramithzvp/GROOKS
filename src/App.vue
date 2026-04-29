f241971c05457294d79d17ad76a29b726af87d82
pramithzvp <pramithvidanapathirana@gmail.com>
2026-04-29 14:22:23 +0530

    spa project gui

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Footer from './components/Footer.vue'
 <template>
   <div class="min-h-screen flex flex-col font-sans">
     <Header />
-    <main class="flex-grow bg-gray-100 dark:bg-gray-800 transition-colors duration-200">
+    <main class="grow bg-gray-100 dark:bg-gray-800 transition-colors duration-200">
       <RouterView />
     </main>
     <Footer />
--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.postcss' {
+  const content: any
+  export default content
+}
